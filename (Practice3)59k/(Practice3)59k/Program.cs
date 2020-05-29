b06f5ba35c846a6bfa4497f4c0f717c1ce53f21d
aschaschukhina00 <63203419+aschaschukhina00@users.noreply.github.com>
2020-05-29 15:28:25 +0500

    Add files via upload

---
+++ (Practice3)59k/(Practice3)59k/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            bool tr;
+
+            do
+            {
+                Console.WriteLine("Введите кординату Х");
+                tr = double.TryParse(Console.ReadLine(), out x);
+                if (!tr)
+                    Console.WriteLine("Введено недопустимое значение");
+
+            }
+            while (!tr);
+
+            do
+            {
+                Console.WriteLine("Введите координату Y");
+                tr = double.TryParse(Console.ReadLine(), out y);
+                if (!tr)
+                    Console.WriteLine("Введено недопустимое значение");
+            }
+            while (!true);
+
+            bool output = isOwned(x, y);
+            if (output)
+            {
+                Console.WriteLine("Точка (" + x + ";" + y + ") принадлежит заданной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка (" + x + ";" + y + ") не принадлежит заданной области");
+            }
+        }
+
+        public static bool isOwned(double x, double y)
+        {
+            if ((y>=1) || (x >= -1 && y >= x) || (x<=1 && y>=x))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
---
+++ (Practice3)59k/(Practice3)59k/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("(Practice3)59k")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("(Practice3)59k")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("17d48d40-19d5-4fe4-b3de-2c7f6a8a244d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
