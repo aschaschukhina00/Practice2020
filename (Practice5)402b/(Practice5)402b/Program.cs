014cff8d526cc8b8069d62247385d4657729517b
aschaschukhina00 <63203419+aschaschukhina00@users.noreply.github.com>
2020-06-14 21:51:01 +0500

    Update Program.cs

--- (Practice5)402b/(Practice5)402b/Program.cs
+++ (Practice5)402b/(Practice5)402b/Program.cs
@@ -66,7 +66,7 @@ namespace _Practice5_402b
                 Console.Write("Введите n: ");
                 ok = Int32.TryParse(Console.ReadLine(), out n);
                 if (!ok || n < 2)
-                    Console.WriteLine("Ошибка! Введите натуральноe число, которое больше или равно 2");
+                    Console.WriteLine("Ошибка! Введите натуральноe число больше 1");
             } while (!ok || n < 2);
             return n;
         }
